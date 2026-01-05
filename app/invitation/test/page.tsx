7102eb2bad625c41f875897ce751dcab4e501296
kusmiderdaniel <kusmiderdaniel@gmail.com>
2026-01-05 11:53:46 +0100

    debug: Add simple test page for invitation route

---
+++ app/invitation/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Invitation Test Page</h1>
+      <p>If you can see this, the routing works!</p>
+    </div>
+  )
+}
